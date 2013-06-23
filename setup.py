280be3156180b37f32776cb00cc0b011adc47e0f
Georges Toth <georges@trypill.org>
2013-06-23 23:43:58 +0200

    move files to subfolder; add debian pkg file; add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='eml_parser',
+  version = '0.5',
+  description='Python EML parser library',
+  author='Georges Toth',
+  author_email='georges@trypill.org',
+  packages = ['eml_parser'],
+  use_2to3 = True,
+)
