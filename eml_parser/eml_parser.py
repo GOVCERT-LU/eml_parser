a7716db11cc71835840a3685392044658f6bfd68
Paul Jung <paul.jung@ext.govcert.etat.lu>
2017-03-20 16:25:06 +0100

    Work on #40, temporary python2 fix

--- eml_parser/eml_parser.py
+++ eml_parser/eml_parser.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long
-# from __future__ import print_function
-from __future__ import absolute_import, division, print_function, unicode_literals
+
+from __future__ import absolute_import, division, print_function
 
 #
 # Georges Toth (c) 2013-2014 <georges@trypill.org>
@@ -433,7 +433,7 @@ def decode_value(string):
 
 def decode_email(eml_file, include_raw_body=False, include_attachment_data=False, pconf=False):
     with open(eml_file) as fp:
-      msg = email.message_from_file(fp)
+        msg = email.message_from_file(fp)
     return parse_email(msg, include_raw_body, include_attachment_data, pconf)
 
 
@@ -467,7 +467,19 @@ def headeremail2list(mail, header):
 
 
 # Iterator that give all position of a given pattern (no regex)
+# FIXME :
+# Error may occurs when using unicode-literals or python 3 on dirty emails
+# Need to check if buffer is a clean one 
+# may be tested with this byte code:
+# -> 00000b70  61 6c 20 32 39 b0 20 6c  75 67 6c 69 6f 20 32 30  |al 29. luglio 20|
+# Should crash on "B0".
 def findall(pat, data):
+    """
+    Iterators that find a pattern
+    Args:
+        pat (str): Pattern to seek
+        data (str): buffer
+    """
     i = data.find(pat)
     while i != -1:
         yield i
