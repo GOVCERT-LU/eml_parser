0a6c0b16f33b79c5b98617b25e4dc4307331634f
Georges Toth <georges.toth@govcert.etat.lu>
2019-11-30 21:34:52 +0100

    fixes #39

--- examples/recursively_extract_attachments.py
+++ examples/recursively_extract_attachments.py
@@ -1,37 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 #
 # Simple example showing how to parse all .eml files in the current folder
 # and extract any attachments to a pre-configured folder
 #
 
-import os
 import base64
-import eml_parser
+import os
 
+import eml_parser
 
 # where to save attachments to
 outpath = '.'
-
 
 for k in os.listdir('.'):
     if k.endswith('.eml'):
         print('Parsing: {}'.format(k))
 
-        m = eml_parser.decode_email(k, include_attachment_data=True)
+        m = eml_parser.eml_parser.decode_email(k, include_attachment_data=True)
 
-        for a_id, a in m['attachments'].items():
-            if a['filename'] == '':
-                filename = a_id
-            else:
-                filename = a['filename']
+        if 'attachments' in m:
+            for a_id, a in m['attachments'].items():
+                if a['filename'] == '':
+                    filename = a_id
+                else:
+                    filename = a['filename']
 
-            filename = os.path.join(outpath, filename)
+                filename = os.path.join(outpath, filename)
 
-            print('\tWriting attachment: {}'.format(filename))
-            with open(filename, 'wb') as a_out:
-                a_out.write(base64.b64decode(a['raw']))
+                print('\tWriting attachment: {}'.format(filename))
+                with open(filename, 'wb') as a_out:
+                    a_out.write(base64.b64decode(a['raw']))
 
-        print()
+            print()
