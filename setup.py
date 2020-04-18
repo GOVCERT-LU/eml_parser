f4cb48458280baecf0410e454828b95352aeccff
Georges Toth <georges.toth@govcert.etat.lu>
2020-04-18 02:51:54 +0200

    update cicd stuff; convert to setup.py and pyproject.toml
    Squashed commit of the following:
    
    commit 91e3b9c29caf2c09a5cffbf86b59b026b8f558dc
    Author: Georges Toth <georges.toth@govcert.etat.lu>
    Date:   Sat Apr 18 02:50:20 2020 +0200
    
    commit 6a9d20251f910833322676fb38989aaa12b7a1ad
    Author: Georges Toth <georges.toth@govcert.etat.lu>
    Date:   Sat Apr 18 02:44:27 2020 +0200
    
    commit 2c1a4607f700595e9932822f0249e937e6588c15
    Author: Georges Toth <georges.toth@govcert.etat.lu>
    Date:   Sat Apr 18 02:42:36 2020 +0200
    
    commit 0c34c055d0fbf31a77f061fce99016c0b21eee69
    Author: Georges Toth <georges.toth@govcert.etat.lu>
    Date:   Sat Apr 18 02:42:00 2020 +0200
    
    commit 4a63eafa6224e6d387d381a673e97caaecaf432a
    Author: Georges Toth <georges.toth@govcert.etat.lu>
    Date:   Sat Apr 18 02:31:07 2020 +0200
    
    commit 53289559c853522f376c1b2a9e61122df89901da
    Author: Georges Toth <georges.toth@govcert.etat.lu>
    Date:   Sat Apr 18 02:30:09 2020 +0200

--- setup.py
+++ setup.py
@@ -1,40 +1,5 @@
 # -*- coding: utf-8 -*-
-import os.path
 
 import setuptools
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-long_description = f.read().strip()
-f.close()
-
-install_requires = ['python-dateutil',
-                    'cchardet',
-                    'typing; python_version < "3.5"'
-                    ]
-
-setuptools.setup(name='eml_parser',
-                 description='Python EML parser library',
-                 license='AGPLv3+',
-                 long_description=long_description,
-                 version='1.11.6',
-                 author='Georges Toth',
-                 author_email='georges.toth@govcert.etat.lu',
-                 url='https://github.com/GOVCERT-LU/eml_parser',
-                 keywords='email',
-                 packages=setuptools.find_packages(),
-                 package_data={'eml_parser': ['py.typed']},
-                 classifiers=['Development Status :: 5 - Production/Stable',
-                              'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-                              'Intended Audience :: Developers',
-                              'Operating System :: OS Independent',
-                              'Programming Language :: Python :: 3.5',
-                              'Programming Language :: Python :: 3.6',
-                              'Programming Language :: Python :: 3.7',
-                              'Programming Language :: Python :: Implementation :: CPython',
-                              'Topic :: Communications :: Email'
-                              ],
-                 extras_require={
-                     'file-magic': ["file-magic"]
-                 },
-                 install_requires=install_requires,
-                 )
+setuptools.setup()
