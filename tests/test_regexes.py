a7e5f953842c4fa11a2a09d64bd3c7d693d600c3
malvidin <malvidin@gmail.com>
2021-10-30 15:04:10 +0200

    Add tests for url_regex_simple Change where parens are matched in url_regex_simple Specify which re engine needs which expression

---
+++ tests/test_regexes.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+from __future__ import annotations
+
+import pathlib
+from eml_parser.regexes import *
+
+my_execution_dir = pathlib.Path(__file__).resolve().parent
+parent_dir = my_execution_dir.parent
+samples_dir = pathlib.Path(parent_dir, 'samples')
+
+
+class TestRegularExpressions:
+    def test_url_regex_simple(self):
+        with pathlib.Path(samples_dir, 'regexes_url_samples.txt').open('r', encoding='utf8') as fhdl:
+            url_text_list = fhdl.read().splitlines()
+
+        url_match_list = []
+        for line in url_text_list:
+            if url_regex_simple.search(line):
+                url_match_list.append(line)
+
+        with pathlib.Path(samples_dir, 'regexes_url_matches.txt').open('r', encoding='utf8') as fhdl:
+            url_check_list = [li for li in fhdl.read().splitlines() if len(li) > 3 and not li.startswith('#')]
+
+        url_diff = set(url_match_list) ^ set(url_check_list)
+
+        assert url_diff == set()
